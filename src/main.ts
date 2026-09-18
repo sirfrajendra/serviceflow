ac19f5e9dc259c2418eae50d0b8061e37a27a6d0
sirfrajendra <sirfrajendra@gmail.com>
2026-09-18 15:34:50 +0545

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
@@ -12,6 +13,18 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('ServiceFlow API')
+    .setDescription('ServiceFlow service management API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-await bootstrap();
+
+bootstrap();
